2cd9d5abc1d8e3ab3c55e9dbbd5bedd90148a42a
qoyyima22 <qoyyimafiassalam@gmail.com>
2019-12-23 09:18:08 +0700

    save to sql and scraping ok

--- server/app.go
+++ server/app.go
@@ -1,5 +1,115 @@
 package main
-import "fmt"
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "127.0.0.1"
+	port     = 5432
+	user     = "postgres"
+	password = "Mantap123"
+	dbname   = "cv-data"
+)
+
+type req_body struct {
+	Folder string
+	HTML   string
+	Title  string
+	En     string
+	Id     string
+	Score  int
+}
+
 func main() {
-    fmt.Println("kuntul kamu!")
+	http.HandleFunc("/insert/cv", processRequest)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Halo bro"))
+	})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func processRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var t req_body
+	err := decoder.Decode(&t)
+	if err != nil {
+		log.Println("error: ", err)
+	}
+	println(t.Score)
+
+	w.Write([]byte("Success "))
+	insertToTable(t.Folder, t.Title, t.HTML, t.En, t.Id, t.Score)
+}
+
+func insertToTable(Folder string, Title string, HTML string, En string, Id string, Score int) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	sqlStatement := `
+    INSERT INTO cvs (id, created_at, title, html, cv_en, cv_id, score, folder)
+    VALUES ($1,$2,$3,$4,$5,$6,$7,$8)
+    RETURNING id
+	`
+	id := getLastId() + 1
+	err = db.QueryRow(sqlStatement, id, time.Now(), Title, HTML, En, Id, Score, Folder).
+		Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("new record ID is:", id)
+}
+
+func getLastId() int {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	sqlStatement := `
+	SELECT id
+	FROM cvs
+	ORDER BY id DESC
+	LIMIT 1
+	`
+
+	rows, err := db.Query(sqlStatement)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	defer rows.Close()
+	var id int
+	for rows.Next() {
+		if err := rows.Scan(&id); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return id
 }
--- web-scraping/index.py
+++ web-scraping/index.py
@@ -4,10 +4,13 @@ import pandas as pd
 from googletrans import Translator
 from datetime import datetime
 import os
+import requests
+import re
 
 driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
 
-driver.get("https://www.livecareer.co.uk/cv-search/r/production-liaison-officer-48bd3cd726974e2b882369c5dd4331d4")
+driver.get(
+    "https://www.livecareer.co.uk/cv-search/r/warehouse-operative-eaa2942db7b2467bb9c4f9869d62b608")
 
 content = driver.page_source
 
@@ -15,15 +18,16 @@ soup = BeautifulSoup(content, 'html.parser')
 
 documentHTML = soup.find_all('div', id="document")[0]
 document = documentHTML.get_text()
-score = soup.find_all('h3', class_="resume-score")[0].get_text()
+scoreTemp = soup.find_all('h3', class_="resume-score")[0].get_text()
+score = int(re.findall(r'\d+', scoreTemp)[0])
 title = soup.find_all('h1', class_="h1")[0].get_text()
 
 ts = Translator()
 
-documentbahasa = ts.translate(document, dest="id")
+documentbahasa = ts.translate(document, dest="id").text
 
 folder_name = datetime.now().strftime(
-    "%H:%M:%S--%d-%b-%Y" + "--" + title + ".txt")
+    "%H:%M:%S--%d-%b-%Y" + "--" + title)
 
 os.mkdir("../cv-data/"+folder_name)
 
@@ -36,16 +40,18 @@ fileEN.write(document)
 fileEN.close()
 
 fileID = open("../cv-data/"+folder_name+"/id.txt", "w+")
-fileID.write(documentbahasa.text)
+fileID.write(documentbahasa)
 fileID.close()
 
-
 fileScore = open("../cv-data/"+folder_name+"/score.txt", "w+")
-fileScore.write(score)
+fileScore.write(str(score))
 fileScore.close()
 
 driver.close()
 
+r = requests.post('http://localhost:8080/insert/cv',
+                  json={"Folder": folder_name, "Title": title, "HTML": "", "En": document, "Id": documentbahasa, "Score": score})
+print(r, "successfully send data")
 
 # Full HTML Page Source
 
